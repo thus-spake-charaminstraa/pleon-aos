76a4bf58f0522683f459338c4ba1e1f37cc799c9
ulalala <reyoula@gmail.com>
2022-06-27 18:18:47 +0900

    path edit

--- app/src/main/java/com/charaminstra/pleon/adapters/PleonPagerAdapter.kt
+++ app/src/main/java/com/charaminstra/pleon/adapters/PleonPagerAdapter.kt
@@ -1,7 +1,11 @@
-package com.charaminstra.pleon
+package com.charaminstra.pleon.adapters
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.charaminstra.pleon.CameraFragment
+import com.charaminstra.pleon.FeedFragment
+import com.charaminstra.pleon.GardenFragment
+import com.charaminstra.pleon.MyFragment
 
 const val FEED_PAGE_INDEX = 0
 const val CAMERA_PAGE_INDEX = 1
@@ -11,8 +15,8 @@ const val MY_PAGE_INDEX = 3
 class PleonPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
     private val tabFragmentsCreators: Map<Int, () -> Fragment> = mapOf(
         FEED_PAGE_INDEX to { FeedFragment() },
-        CAMERA_PAGE_INDEX to { CameraFragment()},
-        GARDEN_PAGE_INDEX to { GardenFragment()},
+        CAMERA_PAGE_INDEX to { CameraFragment() },
+        GARDEN_PAGE_INDEX to { GardenFragment() },
         MY_PAGE_INDEX to { MyFragment() }
     )
 
