b0cdc331cb360ef1fbba6af5546cea88dc4ed6ac
ulalala <reyoula@gmail.com>
2022-06-28 17:16:33 +0900

    PLEON-19 preferenceUtill

--- app/src/main/java/com/charaminstra/pleon/MainActivity.kt
+++ app/src/main/java/com/charaminstra/pleon/MainActivity.kt
@@ -2,10 +2,18 @@ package com.charaminstra.pleon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        lateinit var prefs: PreferenceUtil
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
+        prefs = PreferenceUtil(applicationContext)
         super.onCreate(savedInstanceState)
+        prefs.setString("phone", "010-1234-1234")
+        prefs.setString("token", "012341234")
+//        Log.i("pleon", prefs.getString("phone",""))
         setContentView(R.layout.activity_main)
     }
 }
