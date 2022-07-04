5a8e665476061908dfd807db0675cf3806e407f9
youla <reyoula@gmail.com>
2022-07-04 16:05:31 +0900

    PLEON-30 add hilt

--- app/src/main/java/com/charaminstra/pleon/PleonApplication.kt
+++ app/src/main/java/com/charaminstra/pleon/PleonApplication.kt
@@ -1,7 +1,9 @@
 package com.charaminstra.pleon
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class PleonApplication: Application() {
     override fun onCreate() {
         super.onCreate()
