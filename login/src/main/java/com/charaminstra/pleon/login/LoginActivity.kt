40016d23c7ac3f0b3ffd6f8eaddadd23efb0d45b
ulalala <reyoula@gmail.com>
2022-07-07 16:26:07 +0900

    PLEON-30 dependencies move

--- login/src/main/java/com/charaminstra/pleon/login/LoginActivity.kt
+++ login/src/main/java/com/charaminstra/pleon/login/LoginActivity.kt
@@ -2,8 +2,6 @@ package com.charaminstra.pleon.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavDestination
-import androidx.navigation.Navigation
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
