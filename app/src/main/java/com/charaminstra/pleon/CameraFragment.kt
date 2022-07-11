086aecb88b8a86968676d4d9e33f2f21e8ddb8a2
youla <reyoula@gmail.com>
2022-07-11 17:15:10 +0900

    PLEON-30 camera

---
+++ app/src/main/java/com/charaminstra/pleon/CameraFragment.kt
@@ -0,0 +1,29 @@
+package com.charaminstra.pleon
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.charaminstra.pleon.databinding.FragmentCameraBinding
+
+class CameraFragment : Fragment() {
+    private var _binding: FragmentCameraBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCameraBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+}
