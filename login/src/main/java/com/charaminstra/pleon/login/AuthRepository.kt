1107e1524cbc54610bea59376f50e481a6238ded
ulalala <reyoula@gmail.com>
2022-07-13 18:31:30 +0900

    PLEON-21 bearer

--- login/src/main/java/com/charaminstra/pleon/login/AuthRepository.kt
+++ login/src/main/java/com/charaminstra/pleon/login/AuthRepository.kt
@@ -6,6 +6,6 @@ import com.charaminstra.pleon.login.SplashActivity.Companion.prefs
 import javax.inject.Inject
 
 class AuthRepository @Inject constructor(private val service: APIInterface)  {
-    suspend fun getAuth() = service.getAuth(accessToken = prefs.getAccessToken())
+    suspend fun getAuth() = service.getAuth(accessToken = "Bearer "+prefs.getAccessToken())
 
 }
