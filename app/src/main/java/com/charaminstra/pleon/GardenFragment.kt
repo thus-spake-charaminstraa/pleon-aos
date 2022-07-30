4a4621fb146b7663fd0f03a899f86c347465887a
ulalala <reyoula@gmail.com>
2022-07-31 01:52:11 +0900

    adapter 문제 해결

--- app/src/main/java/com/charaminstra/pleon/GardenFragment.kt
+++ app/src/main/java/com/charaminstra/pleon/GardenFragment.kt
@@ -3,10 +3,10 @@ package com.charaminstra.pleon
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
@@ -21,40 +21,54 @@ import dagger.hilt.android.AndroidEntryPoint
 class GardenFragment : Fragment() {
     private val viewModel: PlantsViewModel by viewModels()
     private val authViewModel: AuthViewModel by viewModels()
-    private lateinit var binding : FragmentGardenBinding
+    private lateinit var binding: FragmentGardenBinding
     private lateinit var navController: NavController
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = FragmentGardenBinding.inflate(layoutInflater)
-        navController = this.findNavController()
-        viewModel.getData().observe(this, Observer {
-            val adapter = CommonAdapter(it,"GARDEN_PLANT",onItemClicked = { plantId->
-                val bundle = Bundle()
-                bundle.putString("id",plantId)
-                navController.navigate(R.id.view_pager_fragment_to_plant_detail_fragment,bundle)})
-            binding.recyclerview.adapter = adapter
-        })
+    private lateinit var adapter: CommonAdapter
 
-        authViewModel.getName().observe(this, Observer {
-            binding.gardenFragmentTitle.text = it + "님의 Garden"
-        })
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        binding = FragmentGardenBinding.inflate(layoutInflater)
+        navController = this.findNavController()
 
         binding.circleBtn.setOnClickListener {
             val intent = Intent(context, PlantRegisterActivity::class.java)
-            intent.putExtra("from","main")
+            intent.putExtra("from", "main")
             startActivity(intent)
         }
         return binding.root
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initList()
+        observeViewModel()
+        binding.recyclerview.adapter = adapter
+    }
+
+    private fun initList() {
+        adapter = CommonAdapter()
+        adapter.setType("GARDEN_PLANT")
+        adapter.onItemClicked = { plantId ->
+            val bundle = Bundle()
+            bundle.putString("id", plantId)
+            navController.navigate(R.id.view_pager_fragment_to_plant_detail_fragment, bundle)
+        }
+    }
+
+    private fun observeViewModel() {
+        authViewModel.getName().observe(viewLifecycleOwner, Observer {
+            binding.gardenFragmentTitle.text = it + "님의 Garden"
+        })
+
+        viewModel.plantsList.observe(viewLifecycleOwner, Observer {
+            adapter.refreshItems(it)
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
         //viewmodel update
         viewModel.loadData()
     }
--- app/src/main/java/com/charaminstra/pleon/adapter/CommonAdapter.kt
+++ app/src/main/java/com/charaminstra/pleon/adapter/CommonAdapter.kt
@@ -1,15 +1,24 @@
 package com.charaminstra.pleon.adapter
 
-import android.util.Log
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.charaminstra.pleon.PlantViewHolderFactory
 import com.charaminstra.pleon.foundation.model.PlantDataObject
 import com.charaminstra.pleon.viewholder.CommonViewHolder
 
-class CommonAdapter(private val viewItemList: List<PlantDataObject>,
-                    private val type: String,
-                    private val onItemClicked: (String)-> Unit): RecyclerView.Adapter<CommonViewHolder>() {
+
+//class CommonAdapter(private val type: String,
+//                    private val onItemClicked: (String)-> Unit): RecyclerView.Adapter<CommonViewHolder>() {
+class CommonAdapter(): RecyclerView.Adapter<CommonViewHolder>() {
+
+    //var viewItemList = mutableListOf<PlantDataObject>()
+//
+    var viewItemList: List<PlantDataObject> = listOf()
+    //var viewItemList  = ArrayList<PlantDataObject>()
+    private lateinit var type: String
+    var onItemClicked: (String)-> Unit = {}
+
+
     override fun getItemViewType(position: Int): Int {
         return CommonViewType.valueOf(type).ordinal
     }
@@ -21,8 +30,19 @@ class CommonAdapter(private val viewItemList: List<PlantDataObject>,
     override fun getItemCount(): Int {
         return viewItemList.size
     }
+
     override fun onBindViewHolder(holder: CommonViewHolder, position: Int) {
         holder.bind(viewItemList[position])
         holder.itemView.setOnClickListener { onItemClicked(viewItemList[position].id!!) }
     }
+
+    fun refreshItems(viewItemList : List<PlantDataObject>) {
+        this.viewItemList = viewItemList
+        notifyDataSetChanged() // Andoid RecyclerView DiffUtil.
+    }
+
+    fun setType(type: String) {
+        this.type = type
+    }
+
 }
