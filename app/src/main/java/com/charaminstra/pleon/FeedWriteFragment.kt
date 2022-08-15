b748440e0ea096437506cfc10fa741960eda859d
youla <reyoula@gmail.com>
2022-08-15 17:07:50 +0900

    PLEON-79 feed write date picker

--- app/src/main/java/com/charaminstra/pleon/FeedWriteFragment.kt
+++ app/src/main/java/com/charaminstra/pleon/FeedWriteFragment.kt
@@ -99,6 +99,7 @@ class FeedWriteFragment : Fragment() {
         }
         binding.imageCard.visibility = View.GONE
         binding.cameraBtn.setOnClickListener{
+            binding.contentEdit.hideKeyboard()
             popUpImageMenu(it)
             binding.imageCard.visibility = View.VISIBLE
         }
@@ -204,10 +205,14 @@ class FeedWriteFragment : Fragment() {
     }
 
     fun popUpCalendar(view: TextView) {
-        var datePickerDialog = DatePickerDialog(requireContext(), { _, y, m, d ->
-            cal.set(y,m,d)
-            view.text = dateFormat.format(cal.time)
-        }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).apply {
+        val cal = Calendar.getInstance()
+        dateFormat = SimpleDateFormat(resources.getString(com.charaminstra.pleon.common_ui.R.string.date_format))
+        var datePickerDialog = DatePickerDialog(requireContext(),
+            com.charaminstra.pleon.common_ui.R.style.PleonDatePickerStyle,
+            { _, y, m, d ->
+                cal.set(y,m,d)
+                view.text = dateFormat.format(cal.time)
+            }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).apply {
             datePicker.maxDate = cal.timeInMillis
         }
         datePickerDialog.show()
