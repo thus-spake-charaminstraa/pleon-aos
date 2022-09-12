de09d93bd4b3e5abe5429bd979854081a98f4833
ulalala <reyoula@gmail.com>
2022-09-12 21:04:17 +0900

    PLEON-90 date picker spinner add

--- plant-register/src/main/java/com/charaminstra/pleon/plant_register/ui/PlantAdoptFragment.kt
+++ plant-register/src/main/java/com/charaminstra/pleon/plant_register/ui/PlantAdoptFragment.kt
@@ -1,15 +1,21 @@
 package com.charaminstra.pleon.plant_register.ui
 
+import android.app.DatePickerDialog
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.charaminstra.pleon.common.showKeyboard
 import com.charaminstra.pleon.common_ui.CustomDialog
+import com.charaminstra.pleon.common_ui.DatepickerDialog
 import com.charaminstra.pleon.common_ui.showErrorToast
 import com.charaminstra.pleon.plant_register.PlantRegisterViewModel
 import com.charaminstra.pleon.plant_register.R
@@ -18,12 +24,15 @@ import com.charaminstra.pleon.plant_register.databinding.FragmentPlantNameBindin
 import com.charaminstra.pleon.plant_register.databinding.FragmentPlantRegisterBinding
 import com.charaminstra.pleon.plant_register.databinding.FragmentPlantWaterBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.*
 
 @AndroidEntryPoint
 class PlantAdoptFragment : Fragment() {
     private val TAG = javaClass.name
     private val viewModel: PlantRegisterViewModel by activityViewModels()
     private lateinit var binding: FragmentPlantAdoptBinding
+    private lateinit var dateFormat: SimpleDateFormat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +50,12 @@ class PlantAdoptFragment : Fragment() {
 
         binding.plantAdoptBackBtn.setOnClickListener {
             navController.popBackStack()
+        }
+
+        binding.plantAdoptEt.setOnClickListener {
+            Log.i("CLICK","ETET CLCICK")
+            //DatepickerDialog(requireContext())
+            popUpCalendar()
         }
 
         binding.plantAdoptNextBtn.setOnClickListener {
@@ -69,6 +84,23 @@ class PlantAdoptFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    fun popUpCalendar(){
+        val edialog : LayoutInflater = LayoutInflater.from(context)
+        val mView : View = edialog.inflate(com.charaminstra.pleon.common_ui.R.layout.dialog_datepicker,null)
+        val cal = Calendar.getInstance()
+        dateFormat = SimpleDateFormat(resources.getString(com.charaminstra.pleon.common_ui.R.string.date_view_format))
+        var datePickerDialog = DatePickerDialog(requireContext(),
+            com.charaminstra.pleon.common_ui.R.style.PleonDatePickerStyle,
+            { _, y, m, d ->
+                cal.set(y,m,d)
+                binding.plantAdoptEt.setText(dateFormat.format(cal.time))
+            }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).apply {
+            datePicker.maxDate = cal.timeInMillis
+        }
+        datePickerDialog.setView(mView)
+        datePickerDialog.show()
     }
 
 }
