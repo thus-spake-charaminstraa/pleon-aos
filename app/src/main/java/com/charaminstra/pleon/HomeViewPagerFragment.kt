5b0e2800618f3215f52bc6f1667f5162eb3d1862
ulalala <reyoula@gmail.com>
2022-09-15 17:19:41 +0900

    PLEON-92 tab layout to bottom navigation

--- app/src/main/java/com/charaminstra/pleon/HomeViewPagerFragment.kt
+++ app/src/main/java/com/charaminstra/pleon/HomeViewPagerFragment.kt
@@ -5,8 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.charaminstra.pleon.databinding.FragmentViewPagerBinding
-import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeViewPagerFragment : Fragment() {
     override fun onCreateView(
@@ -15,25 +15,49 @@ class HomeViewPagerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View{
         val binding = FragmentViewPagerBinding.inflate(inflater, container, false)
-        val tabLayout = binding.tabs
         val viewPager = binding.viewPager
         viewPager.adapter = PleonPagerAdapter(this)
         viewPager.isUserInputEnabled = false
 
-        // Set the icon and text for each tab
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.setIcon(getTabIcon(position))
-        }.attach()
-        return binding.root
-    }
-    private fun getTabIcon(position: Int): Int {
-        return when (position) {
-            FEED_PAGE_INDEX -> R.drawable.feed_tab_selector
-            CHAT_PAGE_INDEX -> R.drawable.chat_tab_selector
-            GARDEN_PAGE_INDEX -> R.drawable.garden_tab_selector
-            MY_PAGE_INDEX -> R.drawable.my_tab_selector
-            else -> throw IndexOutOfBoundsException()
+        binding.bottomNavigation.itemIconTintList = null
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {}
+            override fun onPageSelected(position: Int) {
+                binding.bottomNavigation.menu.getItem(position).isChecked=true
+            }
+        })
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.feed -> {
+                    binding.viewPager.currentItem = FEED_PAGE_INDEX
+                }
+                R.id.chat -> {
+                    binding.viewPager.currentItem = CHAT_PAGE_INDEX
+                }
+                R.id.doctor -> {
+                    binding.viewPager.currentItem = DOCTOR_PAGE_INDEX
+                }
+                R.id.garden -> {
+                    binding.viewPager.currentItem = GARDEN_PAGE_INDEX
+                }
+                R.id.my -> {
+                    binding.viewPager.currentItem = MY_PAGE_INDEX
+                }
+                else -> {
+                    false
+                }
+            }
+            true
         }
+        return binding.root
     }
 }
 
--- app/src/main/java/com/charaminstra/pleon/PleonPagerAdapter.kt
+++ app/src/main/java/com/charaminstra/pleon/PleonPagerAdapter.kt
@@ -3,20 +3,23 @@ package com.charaminstra.pleon
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.charaminstra.pleon.chat.ChatFragment
+import com.charaminstra.pleon.view.CameraFragment
 import com.charaminstra.pleon.view.FeedFragment
 import com.charaminstra.pleon.view.GardenFragment
 import com.charaminstra.pleon.view.MyFragment
 
 const val FEED_PAGE_INDEX = 0
 const val CHAT_PAGE_INDEX = 1
-const val GARDEN_PAGE_INDEX = 2
-const val MY_PAGE_INDEX = 3
+const val DOCTOR_PAGE_INDEX = 2
+const val GARDEN_PAGE_INDEX = 3
+const val MY_PAGE_INDEX = 4
 
 class PleonPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
     private val tabFragmentsCreators: Map<Int, () -> Fragment> = mapOf(
         FEED_PAGE_INDEX to { FeedFragment() },
         CHAT_PAGE_INDEX to { ChatFragment() },
+        DOCTOR_PAGE_INDEX to { CameraFragment()} ,
         GARDEN_PAGE_INDEX to { GardenFragment() },
         MY_PAGE_INDEX to { MyFragment() }
     )
