c57fdf187065e297f3c2be62944d25fbf5c5e570
ulalala <reyoula@gmail.com>
2022-09-30 15:53:45 +0900

    PLEON-87 add pleon analytics

---
+++ common/src/main/java/com/charaminstra/pleon/common/PleonAnalytics.kt
@@ -0,0 +1,11 @@
+package com.charaminstra.pleon.common
+
+
+const val CLASS_NAME = "class_name"
+
+//feed fragment
+const val FEED_VIEW = "feed_view"
+const val FEED_WRITE_BTN_CLICK = "feed_write_btn_click"
+const val NOTI_LATER_BTN_CLCIK = "noti_later_btn_click"
+const val NOTI_COMPLETE_BTN_CLCIK = "noti_complete_btn_click"
+const val FEED_FILTER_BTN_CLICK = "feed_filter_btn_click"
