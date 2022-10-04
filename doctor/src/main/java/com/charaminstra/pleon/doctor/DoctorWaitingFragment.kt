d38fca031b68c6fbea86dee5e6e72262c9124232
youla <reyoula@gmail.com>
2022-10-04 21:47:24 +0900

    PLEON-117 prescription ui

---
+++ doctor/src/main/java/com/charaminstra/pleon/doctor/DoctorWaitingFragment.kt
@@ -0,0 +1,50 @@
+package com.charaminstra.pleon.doctor
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DoctorWaitingFragment : Fragment() {
+    private val TAG = javaClass.name
+    //private val viewModel: PlantRegisterViewModel by activityViewModels()
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        navController = this.findNavController()
+        //viewModel.postPlantDetectionModel()
+        return inflater.inflate(R.layout.fragment_doctor_waiting, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        viewModel.plantDetectionSuccess.observe(viewLifecycleOwner, Observer{
+//            if(it == true ){
+//                navController.navigate(R.id.plant_detection_waiting_fragment_to_plant_detection_result_fragment)
+//                viewModel.clearPlantDetectionSuccess()
+//            }else if(it == false){
+//                navController.navigate(R.id.plant_detection_waiting_fragment_to_plant_detection_error_fragment)
+//                viewModel.clearPlantDetectionSuccess()
+//            }else{
+//
+//            }
+//        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
