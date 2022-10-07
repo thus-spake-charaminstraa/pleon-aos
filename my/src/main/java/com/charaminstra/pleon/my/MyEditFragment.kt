d16f3ba8c1eeadd8dba5ed75788d9506087e85dd
youla <reyoula@gmail.com>
2022-10-07 18:04:25 +0900

    my tab set indicator

--- my/src/main/java/com/charaminstra/pleon/my/MyEditFragment.kt
+++ my/src/main/java/com/charaminstra/pleon/my/MyEditFragment.kt
@@ -2,7 +2,9 @@ package com.charaminstra.pleon.my
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
@@ -13,8 +15,10 @@ import android.view.ViewGroup
 import androidx.core.content.FileProvider
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.certified.customprogressindicatorlibrary.CustomProgressIndicator
 import com.charaminstra.pleon.common.PLeonImageFile
 import com.charaminstra.pleon.common.REQUEST_GALLERY
 import com.charaminstra.pleon.common.REQUEST_TAKE_PHOTO
@@ -22,16 +26,21 @@ import com.charaminstra.pleon.common.RequestPermission
 import com.charaminstra.pleon.common_ui.ErrorToast
 import com.charaminstra.pleon.common_ui.PopUpImageMenu
 import com.charaminstra.pleon.my.databinding.FragmentMyEditBinding
+import com.charaminstra.pleon.plant_register.getOrientation
 import dagger.hilt.android.AndroidEntryPoint
-import java.io.FileInputStream
 
 @AndroidEntryPoint
 class MyEditFragment : Fragment() {
     private val TAG = javaClass.name
     private val viewModel: MyViewModel by viewModels()
     private lateinit var binding : FragmentMyEditBinding
+    private lateinit var navController: NavController
+
     private lateinit var permissionMsg: ErrorToast
     lateinit var photoFile: PLeonImageFile
+    lateinit var myUri: Uri
+    lateinit var indicator: CustomProgressIndicator
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         /*카메라권한요청*/
@@ -43,7 +52,8 @@ class MyEditFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMyEditBinding.inflate(inflater)
-        val navController = this.findNavController()
+        setIndicator()
+        navController = this.findNavController()
         permissionMsg = ErrorToast(requireContext())
         binding.myEditBackBtn.setOnClickListener {
             navController.popBackStack()
@@ -58,13 +68,26 @@ class MyEditFragment : Fragment() {
         viewModel.getUserData()
     }
 
+    override fun onPause() {
+        super.onPause()
+        indicator.stopAnimation()
+    }
+
     private fun initListeners(){
         binding.myEditCompleteBtn.setOnClickListener {
             if(binding.userNameInput.text.isNullOrEmpty()){
                 ErrorToast(requireContext()).showMsg(resources.getString(R.string.my_edit_fragment_name_error),binding.userNameInput.y)
-            }else{
-                viewModel.updateUserData(binding.userNameInput.text.toString())
-            }
+            }else if(viewModel.imgBitmap.value != null){
+                indicator.apply {
+                    visibility = View.VISIBLE
+                    startAnimation()
+                }
+                viewModel.myBitmapToUrl()
+                viewModel.imgEdit = true
+                }else{
+                    viewModel.updateUserData(binding.userNameInput.text.toString())
+                }
+
         }
         binding.editUserImage.setOnClickListener{
             val imageMenuDlg = PopUpImageMenu(requireContext())
@@ -87,16 +110,23 @@ class MyEditFragment : Fragment() {
             binding.userNameInput.setText(it)
         })
         viewModel.urlResponse.observe(viewLifecycleOwner, Observer{
-            Glide.with(binding.root)
-                .load(it)
-                .into(binding.editUserImage)
+            if(!viewModel.imgEdit){
+                Glide.with(binding.root)
+                    .load(it)
+                    .into(binding.editUserImage)
+            }else{
+                viewModel.updateUserData(binding.userNameInput.text.toString())
+            }
         })
         viewModel.updateUserDataSuccess.observe(viewLifecycleOwner, Observer {
             if(it){
-                findNavController().popBackStack()
+                navController.popBackStack()
             }else{
                 /* 수정 실패 */
             }
+        })
+        viewModel.imgBitmap.observe(viewLifecycleOwner, Observer {
+            binding.editUserImage.setImageBitmap(it)
         })
     }
 
@@ -109,16 +139,31 @@ class MyEditFragment : Fragment() {
         when (requestCode) {
             REQUEST_GALLERY -> {
                 data ?: return
-                val uri = data.data as Uri
-                activity?.contentResolver?.openInputStream(uri).let {
-                    val bitmap = BitmapFactory.decodeStream(it)
-                    binding.editUserImage.setImageBitmap(bitmap)
-                    viewModel.galleryToUrl(bitmap)
+                myUri = data.data as Uri
+                val inputStream = requireContext().contentResolver?.openInputStream(myUri!!)
+                val bitmap = BitmapFactory.decodeStream(inputStream)
+                val orientation = getOrientation(requireContext(), myUri!!)
+                val matrix = Matrix()
+                when(orientation){
+                    90 -> matrix.postRotate(90F)
+                    180 -> matrix.postRotate(180F)
+                    270 -> matrix.postRotate(270F)
                 }
+                val rotateBitmap = Bitmap.createBitmap(bitmap, 0, 0 ,bitmap.width, bitmap.height, matrix, true)
+                viewModel.setBitmap(rotateBitmap)
             }
             REQUEST_TAKE_PHOTO -> {
-                Glide.with(this).load(photoFile.currentPhotoPath).into(binding.editUserImage)
-                viewModel.cameraToUrl(FileInputStream(photoFile.currentPhotoPath))
+                val inputStream = requireContext().contentResolver?.openInputStream(myUri!!)
+                val bitmap = BitmapFactory.decodeStream(inputStream)
+                val orientation = getOrientation(requireContext(), myUri!!)
+                val matrix = Matrix()
+                when(orientation){
+                    90 -> matrix.postRotate(90F)
+                    180 -> matrix.postRotate(180F)
+                    270 -> matrix.postRotate(270F)
+                }
+                val rotateBitmap = Bitmap.createBitmap(bitmap, 0, 0 ,bitmap.width, bitmap.height, matrix, true)
+                viewModel.setBitmap(rotateBitmap)
             }
             else -> {
                 ErrorToast(requireContext()).showMsg(
@@ -137,15 +182,22 @@ class MyEditFragment : Fragment() {
     private fun openCamera() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         photoFile = PLeonImageFile(requireActivity())
-        val uri = FileProvider.getUriForFile(
+        myUri = FileProvider.getUriForFile(
             requireContext(),
             "com.charaminstra.pleon.fileprovider",
             photoFile.create()
         )
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, myUri)
         startActivityForResult(intent, REQUEST_TAKE_PHOTO)
     }
 
-
+    private fun setIndicator(){
+        indicator = binding.indicator
+        indicator.setProgressIndicatorColor("#8d8d97")
+        indicator.setTrackColor("#35c97a")
+        indicator.setImageResource(com.charaminstra.pleon.common_ui.R.drawable.img_logo)
+        indicator.setText("loading ... ")
+        indicator.setTextSize(15.0F)
+    }
 
 }
