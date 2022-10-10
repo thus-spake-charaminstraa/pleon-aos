ecb1064fa2d1c99eed5a8139bda75266ed9ae487
youla <reyoula@gmail.com>
2022-10-11 02:31:56 +0900

    PLEON-118 second view visibility

--- doctor/src/main/java/com/charaminstra/pleon/doctor/view/PrescriptionFragment.kt
+++ doctor/src/main/java/com/charaminstra/pleon/doctor/view/PrescriptionFragment.kt
@@ -5,56 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.charaminstra.pleon.doctor.DoctorViewModel
 import com.charaminstra.pleon.doctor.R
+import com.charaminstra.pleon.doctor.databinding.FragmentPrescriptionBinding
+import com.charaminstra.pleon.doctor.databinding.FragmentQPlantRegisterBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PrescriptionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PrescriptionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding : FragmentPrescriptionBinding
+    private val viewModel : DoctorViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_prescription, container, false)
+        binding = FragmentPrescriptionBinding.inflate(inflater, container, false)
+
+        initObservers()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PrescriptionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PrescriptionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun initObservers(){
+        viewModel.causesList.observe(viewLifecycleOwner){
+            if(it.size == 1){
+                binding.cause2.root.visibility = View.GONE
+                binding.cause1.causeNum.text = "1"
+                binding.cause1.causeTxt.text = it[0].cause_ko
+
+                binding.solution2.root.visibility = View.GONE
+                binding.solution1.solutionNum.text = "1"
+                binding.solution1.solutionTxt.text = it[0].guide
+            }else if(it.size == 2){
+                binding.cause1.causeNum.text = "1"
+                binding.cause2.causeNum.text = "2"
+                binding.cause1.causeTxt.text = it[0].cause_ko
+                binding.cause2.causeTxt.text = it[1].cause_ko
+                binding.solution1.solutionNum.text = "1"
+                binding.solution2.solutionNum.text = "2"
+                binding.solution1.solutionTxt.text = it[0].guide
+                binding.solution2.solutionTxt.text = it[1].guide
             }
+        }
     }
+
 }
