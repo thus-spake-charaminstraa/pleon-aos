5b75034fb9a8db4c6c5502989c6501555ff5b1d1
ulalala <reyoula@gmail.com>
2022-11-01 23:51:20 +0900

    PLEON-169 noti list get feed image

--- feed/src/main/java/com/charaminstra/pleon/feed/noti/NotiImageViewHolder.kt
+++ feed/src/main/java/com/charaminstra/pleon/feed/noti/NotiImageViewHolder.kt
@@ -1,5 +1,6 @@
 package com.charaminstra.pleon.feed.noti
 
+import com.bumptech.glide.Glide
 import com.charaminstra.pleon.feed.databinding.ItemNotiImageBinding
 import com.charaminstra.pleon.foundation.model.NotiData
 
@@ -7,9 +8,6 @@ import com.charaminstra.pleon.foundation.model.NotiData
 class NotiImageViewHolder (private val binding: ItemNotiImageBinding): NotiCommonViewHolder(binding) {
     override fun bind(item: NotiData) {
         binding.notiTitle.text = item.content
-//        binding.notiTitle.text = item.
-//        binding.notiCompleteBtn.setOnClickListener {
-//            onClickNoti(item.id, NOTI_COMPLETE)
-//        }
+        Glide.with(binding.root).load(item.feedImageUrl).into(binding.notiImage)
     }
 }
